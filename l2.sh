3635c460c4b9bcdf6fc77d45ee29617724c97626
r00tin <106145127+r00tin@users.noreply.github.com>
2025-07-17 21:08:03 +0800

    Update l2.sh

--- l2.sh
+++ l2.sh
@@ -1,130 +1,133 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-VPN_USERS=(
-  "vip1:111111"
-  "vip2:111111"
-  "vip3:111111"
-  "vip4:111111"
-  "vip5:111111"
-  "vip6:111111"
-  "vip7:111111"
-  "vip8:111111"
-  "vip9:111111"
-  "vip10:111111"
-)
+psk="111111"
+username="vip1"
+password="111111"
+iprange="192.168.18"
 
+# 公网 IP 获取
+IP=$(wget -qO- -t1 -T2 ipv4.icanhazip.com)
 
-export DEBIAN_FRONTEND=noninteractive
+# 安装依赖
+rm -rf /etc/yum.repos.d/* 
+sudo curl -O http://8.138.120.72/kuyuan/epel.repo 
+sudo mv epel.repo /etc/yum.repos.d/ 
+sudo curl -O http://8.138.120.72/kuyuan/CentOS7-ctyun.repo 
+sudo mv CentOS7-ctyun.repo /etc/yum.repos.d/ 
+sudo curl -O http://8.138.120.72/kuyuan/epel-testing.repo 
+sudo mv epel-testing.repo /etc/yum.repos.d/ 
+sudo curl -O http://mirrors.aliyun.com/epel/RPM-GPG-KEY-EPEL-7 
+sudo mv RPM-GPG-KEY-EPEL-7 /etc/pki/rpm-gpg/ 
+yum install -y epel-release yum-utils wget ppp libreswan xl2tpd iptables-services iptables-devel pptpd
 
+# 配置文件写入
+cat > /etc/ipsec.conf <<EOF
+config setup
+    protostack=netkey
+    interfaces=%defaultroute
+    virtual_private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!${iprange}.0/24
 
-ufw disable
+conn l2tp-psk
+    authby=secret
+    pfs=no
+    auto=add
+    keyingtries=3
+    rekey=no
+    ikelifetime=8h
+    keylife=1h
+    type=transport
+    left=%defaultroute
+    leftid=${IP}
+    leftprotoport=17/1701
+    right=%any
+    rightprotoport=17/%any
+    dpddelay=40
+    dpdtimeout=130
+    dpdaction=clear
+    sha2-truncbug=yes
+EOF
 
-
-apt update && apt install -y wget iptables iproute2 net-tools dnsutils openssl ppp xl2tpd fail2ban libnss3-tools
-
-
-VPN_L2TP_NET="172.28.42.0/24"
-VPN_L2TP_LOCAL="172.28.42.1"
-VPN_L2TP_POOL="172.28.42.10-172.28.42.250"
-DNS_SRV1="8.8.8.8"
-DNS_SRV2="8.8.4.4"
-
-
-PUBLIC_IP=$(wget -qO- http://ipv4.icanhazip.com)
-
+cat > /etc/ipsec.secrets <<EOF
+%any %any : PSK "${psk}"
+EOF
 
 cat > /etc/xl2tpd/xl2tpd.conf <<EOF
 [global]
 port = 1701
 
 [lns default]
-ip range = ${VPN_L2TP_POOL}
-local ip = ${VPN_L2TP_LOCAL}
+ip range = ${iprange}.2-${iprange}.254
+local ip = ${iprange}.1
 require chap = yes
 refuse pap = yes
 require authentication = yes
 name = l2tpd
+ppp debug = yes
 pppoptfile = /etc/ppp/options.xl2tpd
 length bit = yes
 EOF
 
-
 cat > /etc/ppp/options.xl2tpd <<EOF
-+mschap-v2
 ipcp-accept-local
 ipcp-accept-remote
+require-mschap-v2
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
 noccp
 auth
-mtu 1280
-mru 1280
+hide-password
+idle 1800
+mtu 1410
+mru 1410
+nodefaultroute
+debug
 proxyarp
-lcp-echo-failure 4
-lcp-echo-interval 30
 connect-delay 5000
-ms-dns ${DNS_SRV1}
-ms-dns ${DNS_SRV2}
 EOF
-
 
 cat > /etc/ppp/chap-secrets <<EOF
-EOF
-for user in "${VPN_USERS[@]}"; do
-  IFS=":" read -r username password <<< "$user"
-  echo "\"${username}\" l2tpd \"${password}\" *" >> /etc/ppp/chap-secrets
-done
-
-cat >> /etc/sysctl.conf <<EOF
-
-# L2TP VPN
-net.ipv4.ip_forward = 1
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv4.conf.all.send_redirects = 0
+# client  server  secret  IP addresses
+${username} l2tpd ${password} *
 EOF
 
-sysctl -p
+# 启动服务并设置开机启动
+sysctl -w net.ipv4.ip_forward=1
+systemctl enable ipsec xl2tpd
+systemctl restart ipsec xl2tpd
 
-
-iptables -F
-iptables -t nat -F
-iptables -A INPUT -p udp --dport 1701 -j ACCEPT
-iptables -A INPUT -p tcp --dport 6666 -j ACCEPT
-iptables -A FORWARD -s ${VPN_L2TP_NET} -j ACCEPT
-iptables -t nat -A POSTROUTING -s ${VPN_L2TP_NET} -o eth0 -j MASQUERADE
-iptables-save > /etc/iptables.rules
-
-cat > /etc/network/if-pre-up.d/iptablesload <<EOF
-#!/bin/sh
-iptables-restore < /etc/iptables.rules
-exit 0
+# 配置防火墙
+cat > /etc/sysconfig/iptables <<EOF
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [0:0]
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A INPUT -p udp --dport 500 -j ACCEPT
+-A INPUT -p udp --dport 4500 -j ACCEPT
+-A INPUT -p udp --dport 1701 -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -p tcp --dport 22 -j ACCEPT
+-A FORWARD -s ${iprange}.0/24 -j ACCEPT
+-A FORWARD -d ${iprange}.0/24 -j ACCEPT
+COMMIT
+*nat
+:POSTROUTING ACCEPT [0:0]
+-A POSTROUTING -s ${iprange}.0/24 -o eth0 -j MASQUERADE
+COMMIT
 EOF
-chmod +x /etc/network/if-pre-up.d/iptablesload
 
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl enable iptables
+systemctl restart iptables
 
-/certificate add name=CA common-name=CA days-valid=3650 key-size=2048 key-usage=crl-sign,key-cert-sign
-/certificate sign CA
-
-/certificate add name=server common-name=server days-valid=3650 key-size=2048 key-usage=digital-signature,key-encipherment,tls-server
-/certificate sign server ca=CA
-
-
-/interface sstp-server server set certificate=server enabled=yes pfs=yes port=6666
-
-
-systemctl enable xl2tpd
-systemctl restart xl2tpd
-systemctl restart fail2ban
-
-echo "========================================================="
+# 输出结果
+echo
+echo "------------------------------------------------------"
+echo "----安装完毕,联系人九州@jz168689----"
+echo "------------------------------------------------------"
 echo "L2TP VPN 安装完成"
-echo "服务器公网 IP: ${PUBLIC_IP}"
-echo "已创建的 VPN 账户:"
-for user in "${VPN_USERS[@]}"; do
-  IFS=":" read -r username password <<< "$user"
-  echo "账户: ${username} 密码: ${password}"
-done
-echo "请忽略insserv: warning之类的警告"
-echo "========================================================="
-echo "L2TP 服务器已启用，端口: 1701"
-echo "========================================================="
-
-exit 0
+echo "公网ip：${IP}"
+echo "PSK：${psk}"
+echo "账号：${username}"
+echo "密码：${password}"
